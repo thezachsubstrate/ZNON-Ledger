97be4d7a2d56cbe87f1c08cdb5597fdde7885b32
Zach Mosley <zachmosley@protonmail.com>
2026-01-29 17:03:12 -0500

    Resolve sync.sh merge conflict

--- sync.sh
+++ sync.sh
@@ -3,8 +3,4 @@ cd ~/ZNON-Ledger
 git add .
 git commit -m "Auto-update: $(date '+%Y-%m-%d %H:%M:%S')"
 git push origin main
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 7627edd1d40d56a1bfc7917ed417d7f87327e8ca
